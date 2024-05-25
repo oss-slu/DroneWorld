6611550cfd35b0d17990ccdbd00652e318e7b1ac
Deen <n.salifu@alsoit.io>
2024-05-26 00:36:46 +0100

    add responsive 404 page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,43 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import NotFoundImage from '../Assets/Images/NotFound.svg';
 
 const NotFoundContainer = styled.div`
-  height: 90vh;
+  height: calc(100vh - 5.3em);
   width: 100vw;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  display: block;
+
+  @media (min-width: 768px) {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+`;
+
+const NotFoundImgContainer = styled.div`
+  width: 100%;
   display: flex;
+  align-items: center;
+  padding: 5px;
+
+  @media (min-width: 768px) {
+    width: 50%;
+    padding: 50px;
+    height: 100%;
+  }
+
+  @media (min-width: 600px) and (max-width: 1024px) {
+    padding: 10px;
+  }
+`;
+
+const NotFoundMessage = styled.div`
+  width: 100%;
+  padding: 0;
+
+  @media (min-width: 768px) {
+    width: 50%;
+    padding: 50px;
+  }
+
+  @media (min-width: 600px) and (max-width: 1024px) {
+    padding: 10px;
+  }
+`;
+
+const NotFoundChild = styled.div`
+  width: 90%;
+  margin: auto;
+
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    width: 80%;
+  }
 `;
 
 const RemoveMarginPadding = {
-  margin: 0,
+  margin: '0 0 20px 0',
   padding: 0,
 };
 
 function NotFound() {
   return (
     <NotFoundContainer>
-      <div style={{ textAlign: 'center' }}>
-        <h2 style={{ fontSize: '3em', ...RemoveMarginPadding }}>404 NOT FOUND</h2>
-        <p style={{ fontSize: '1.5em', ...RemoveMarginPadding }}>
-          The page you are looking for might be removed or is temporarily unavailable
-        </p>
-        <Link to='/' style={{ textDecoration: 'none' }}>
-          <Button
-            variant='contained'
-            sx={{
-              color: 'white',
-              padding: '15px 30px',
-              borderRadius: '10px',
-              marginTop: '1rem',
-            }}
-          >
-            Back to Homepage
-          </Button>
-        </Link>
-      </div>
+      <NotFoundImgContainer>
+        <img src={NotFoundImage} alt='' />
+      </NotFoundImgContainer>
+      <NotFoundMessage>
+        <NotFoundChild>
+          <h2 style={{ fontSize: '3em', ...RemoveMarginPadding }}>Something is not right...</h2>
+          <p style={{ fontSize: '1.5em' }}>
+            Page you are trying to open does not exist. You may have mistyped the address, or the
+            page has been moved to another URL. If you think this is an error contact support.
+          </p>
+          <Link to='/' style={{ textDecoration: 'none' }}>
+            <Button
+              variant='contained'
+              sx={{
+                color: 'white',
+                padding: '15px 30px',
+                borderRadius: '10px',
+                marginTop: '1rem',
+              }}
+            >
+              Back to Homepage
+            </Button>
+          </Link>
+        </NotFoundChild>
+      </NotFoundMessage>
     </NotFoundContainer>
   );
 }
