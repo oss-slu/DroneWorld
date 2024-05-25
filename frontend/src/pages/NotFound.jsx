0c0bcccf75e3dd94836932007d908a1c61ff61e0
Deen <n.salifu@alsoit.io>
2024-05-25 13:48:55 +0100

    remove unused imports

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Button } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import { Link } from 'react-router-dom';
 
 const NotFoundContainer = styled.div`
