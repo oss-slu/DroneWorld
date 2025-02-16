30abc224765eb368787b9c55139e80ac31b8da4b
vgalianorouge <valerie.galianorouge@slu.edu>
2025-02-16 17:17:53 -0600

    annotation fix of 'process' not defined

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -2,6 +2,12 @@ export const HOME_LABEL = {
     partfirst: `Simulation of a realistic scenario is an effective way to test the system's requirements.`,
     partsecond: `Describe one or more requirements you would like to test by simulating a scenario`,
   };
+
+  let API_URL = 'http://localhost:8000';
+  if(process.env.REACT_APP_API_URL){
+    API_URL = process.env.REACT_APP_API_URL;
+  }
+  export const BASE_URL = API_URL;
   
   export const imageUrls = {
     drone_icon: '/images/drone-icon.png',
