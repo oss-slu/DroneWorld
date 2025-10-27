589b78f09c2f24f5af6de9b61b4354533e109403
Henry Barsanti <henrybarsanti@gmail.com>
2025-10-27 17:40:24 -0500

    creating new pull request

---
+++ frontend/src/tests/App.test.js
@@ -0,0 +1,9 @@
+/* eslint-env jest */
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders DroneWorld header text', () => {
+  render(<App />);
+  const header = screen.getByText(/DroneWorld/i);
+  expect(header).toBeInTheDocument();
+});
