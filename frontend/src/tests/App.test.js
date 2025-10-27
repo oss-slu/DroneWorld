dddbcb081070b4b22607f3e9227cb78e0b7583b1
Ahmed Bektic <ahmed.bektic@slu.edu>
2025-10-27 15:14:39 -0500

    added aboutus test

--- frontend/src/tests/App.test.js
+++ frontend/src/tests/App.test.js
@@ -4,6 +4,13 @@ import App from '../App';
 
 test('renders DroneWorld header text', () => {
   render(<App />);
-  const header = screen.getByText(/DroneWorld/i);
+  const header = screen.getByText(/Drone World/i);
   expect(header).toBeInTheDocument();
+});
+
+test('About Us link exists and routes to /aboutus', () => {
+  render(<App />);
+  const aboutUsLink = screen.getByRole('link', { name: /about us/i });
+  expect(aboutUsLink).toBeInTheDocument();
+  expect(aboutUsLink).toHaveAttribute('href', '/aboutus');
 });
