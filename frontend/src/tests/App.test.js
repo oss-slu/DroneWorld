921c1e985b58191e4e84253b7a8b53276f2c3e8b
Ahmed Bektic <ahmed.bektic@slu.edu>
2025-11-02 17:56:59 -0600

    Implemented improved test implementation

--- frontend/src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env jest */
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders DroneWorld header text', () => {
-  render(<App />);
-  const header = screen.getByText(/DroneWorld/i);
-  expect(header).toBeInTheDocument();
-});
